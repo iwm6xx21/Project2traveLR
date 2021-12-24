6e80dd581d88b04317472b7e7cc7c0a5fa81b038
ispoon21 <isaiahweathersp@gmail.com>
2021-12-24 14:47:20 -0700

    sign up form connect to mongo database

--- controllers/appsController.js
+++ controllers/appsController.js
@@ -3,32 +3,11 @@ const router = express.Router()
 const Post = require('../models/post')
 
 
-
-
-
-// home route - login and signup page
+// login and sign up route
 router.get('/', (req, res)=> {
     res.render('index')
 
 })
-
-
-// app interface route
-
-// router.get('/login', (req, res) => {
-//     res.render('login')
-// })
-
-
-
-// router.post('/login', (req, res)=> {
-
-// })
-
-
-// router.post('/register', (req, res)=> {
-    
-// })
 
 
 
--- controllers/sessionsController.js
+++ controllers/sessionsController.js
@@ -2,16 +2,22 @@ const express = require('express')
 const router = express.Router()
 const bycrypt = require('bcrypt')
 const User = require('../models/user')
+const { redirect } = require('express/lib/response')
 
 
-
-router.get('/login', (req, res) => {
-    res.render('sessions/login')
+// route hit once user has either logged in to an existing account or registered for a new account
+router.get('/home', (req, res) => {
+    res.render('sessions/home')
     
 })
 
-//
+// route for posting new user information to database and render user to home page. 
 
+router.post('/home', async (req, res) => {
+    const newUser = await User.create(req.body)
+    req.session.username = newUser.username
+    res.render('sessions/home')
+})
 
 
 
